be1eda84950be299e69c114c7a426caee2c12875
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2024-08-05 21:42:31 -0400

    new stuff

--- server.js
+++ server.js
@@ -24,26 +24,7 @@ const io = socketio(server, { // Configure CORS on Socket.IO instance
 })
 
 
-//app.use(express.static('public'));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://skillknightsflask-93e6d75856bb.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 const PORT = process.env.PORT || 3000;
 
